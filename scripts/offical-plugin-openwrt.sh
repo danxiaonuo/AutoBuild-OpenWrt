9428da4d78597a59bcf0e1487bfca98d9a894646
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-04-25 16:26:08 +0800

    Create offical-plugin-openwrt.sh

---
+++ scripts/offical-plugin-openwrt.sh
@@ -0,0 +1,4 @@
+# 增加 lienol passwall
+echo "src-git lienol https://github.com/chenshuo890/lienol-openwrt-package.git" >> feeds.conf.default
+# 增加 helloworld
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
