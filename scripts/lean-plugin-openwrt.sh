1d13133200b4485d975bfee1bd49590bdd1c5769
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-04-25 16:04:47 +0800

    Update lean-plugin-openwrt.sh

--- scripts/lean-plugin-openwrt.sh
+++ scripts/lean-plugin-openwrt.sh
@@ -8,50 +8,50 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lea
 echo "src-git lienol https://github.com/chenshuo890/lienol-openwrt-package.git" >> feeds.conf.default
 
 # smartdns
-svn co https://github.com/pymumu/smartdns/trunk/package/openwrt package/mine/smartdns
-svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-smartdns package/mine/luci-app-smartdns
-#git clone https://github.com/ujincn/smartdns.git package/mine/smartdns
-#git clone https://github.com/ujincn/luci-app-smartdns-compat.git package/mine/luci-app-smartdns-compat
-#git clone https://github.com/pymumu/openwrt-smartdns.git package/mine/smartdns
-#git clone --branch lede https://github.com/pymumu/luci-app-smartdns.git package/mine/luci-app-smartdns
-#svn co https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-smartdns package/mine/luci-app-smartdns
+svn co https://github.com/pymumu/smartdns/trunk/package/openwrt package/danxiaonuo/smartdns
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-smartdns package/danxiaonuo/luci-app-smartdns
+#git clone https://github.com/ujincn/smartdns.git package/danxiaonuo/smartdns
+#git clone https://github.com/ujincn/luci-app-smartdns-compat.git package/danxiaonuo/luci-app-smartdns-compat
+#git clone https://github.com/pymumu/openwrt-smartdns.git package/danxiaonuo/smartdns
+#git clone --branch lede https://github.com/pymumu/luci-app-smartdns.git package/danxiaonuo/luci-app-smartdns
+#svn co https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-smartdns package/danxiaonuo/luci-app-smartdns
 
 # 复杂的AdGuardHome的openwrt的luci界面
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/mine/luci-app-adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/danxiaonuo/luci-app-adguardhome
 
 # 增加openwet常用软件包
-#git clone https://github.com/kenzok8/openwrt-packages.git package/mine/
+#git clone https://github.com/kenzok8/openwrt-packages.git package/danxiaonuo/
 
 # DiskMan for LuCI (WIP)
-# git clone https://github.com/lisaac/luci-app-diskman.git package/mine/luci-app-diskman
-# mkdir -p package/mine/parted && cp -i package/mine/luci-app-diskman/Parted.Makefile package/mine/parted/Makefile
+# git clone https://github.com/lisaac/luci-app-diskman.git package/danxiaonuo/luci-app-diskman
+# mkdir -p package/danxiaonuo/parted && cp -i package/danxiaonuo/luci-app-diskman/Parted.Makefile package/danxiaonuo/parted/Makefile
 
 # KPR plus+
-# git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/mine/luci-app-koolproxyR
+# git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/danxiaonuo/luci-app-koolproxyR
 
 # Server酱
-#git clone https://github.com/tty228/luci-app-serverchan.git package/mine/luci-app-serverchan
+#git clone https://github.com/tty228/luci-app-serverchan.git package/danxiaonuo/luci-app-serverchan
 
 # FileBrowser
-# git clone https://github.com/project-openwrt/FileBrowser.git package/mine/FileBrowser
+# git clone https://github.com/project-openwrt/FileBrowser.git package/danxiaonuo/FileBrowser
 
 # 网易云音乐
-# git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic.git package/mine/luci-app-unblockneteasemusic
+# git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic.git package/danxiaonuo/luci-app-unblockneteasemusic
 
 # OpenClash
-#git clone https://github.com/vernesong/OpenClash.git package/mine/OpenClash
+#git clone https://github.com/vernesong/OpenClash.git package/danxiaonuo/OpenClash
 
 # 网易云音乐GoLang版本
-# git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic-go.git package/mine/luci-app-unblockneteasemusic-go
+# git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic-go.git package/danxiaonuo/luci-app-unblockneteasemusic-go
 
 # 网易云音乐mini
-# git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic-mini.git package/mine/luci-app-unblockneteasemusic-mini
+# git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic-mini.git package/danxiaonuo/luci-app-unblockneteasemusic-mini
 
 # disable usb3.0
-# git clone https://github.com/rufengsuixing/luci-app-usb3disable.git package/mine/luci-app-usb3disable
+# git clone https://github.com/rufengsuixing/luci-app-usb3disable.git package/danxiaonuo/luci-app-usb3disable
 
 # 管控上网行为
-# git clone https://github.com/destan19/OpenAppFilter.git package/mine/OpenAppFilter
+# git clone https://github.com/destan19/OpenAppFilter.git package/danxiaonuo/OpenAppFilter
 
 # Rclone-OpenWrt
-# git clone https://github.com/ElonH/Rclone-OpenWrt.git package/mine/Rclone-OpenWrt
+# git clone https://github.com/ElonH/Rclone-OpenWrt.git package/danxiaonuo/Rclone-OpenWrt
