f80f718fe52bb5e962309786afe792c9b34e7184
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-04-25 18:00:52 +0800

    Update offical-plugin-openwrt.sh

--- scripts/offical-plugin-openwrt.sh
+++ scripts/offical-plugin-openwrt.sh
@@ -2,6 +2,8 @@
 echo "src-git lienol https://github.com/chenshuo890/lienol-openwrt-package.git" >> feeds.conf.default
 # 增加 helloworld
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+# 增加 lede
+echo "src-git lede https://github.com/coolsnowwolf/lede.git" >> feeds.conf.default
 # Add luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon package/danxiaonuo/luci-theme-argon
 # smartdns
