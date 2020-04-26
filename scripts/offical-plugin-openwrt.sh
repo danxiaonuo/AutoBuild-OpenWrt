0f759201f1259de178ad9fddee7735493b374f77
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-04-26 18:19:50 +0800

    Update offical-plugin-openwrt.sh

--- scripts/offical-plugin-openwrt.sh
+++ scripts/offical-plugin-openwrt.sh
@@ -11,6 +11,8 @@ svn co https://github.com/pymumu/smartdns/trunk/package/openwrt package/danxiaon
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-smartdns package/danxiaonuo/luci-app-smartdns
 # 复杂的AdGuardHome的openwrt的luci界面
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/danxiaonuo/luci-app-adguardhome
+Server酱
+git clone https://github.com/tty228/luci-app-serverchan.git package/danxiaonuo/luci-app-serverchan
 # k3设置
 git clone https://github.com/lwz322/luci-app-k3screenctrl.git package/k3/luci-app-k3screenctrl
 git clone https://github.com/lwz322/k3screenctrl.git package/k3/k3screenctrl
