07443cb9e335a0270444dcadedb759ae09c4d23d
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-04-26 22:56:04 +0800

    Update lean-plugin-openwrt.sh

--- scripts/lean-plugin-openwrt.sh
+++ scripts/lean-plugin-openwrt.sh
@@ -17,7 +17,7 @@ svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-
 # 复杂的AdGuardHome的openwrt的luci界面
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/danxiaonuo/luci-app-adguardhome
 
-Server酱
+#Server酱
 git clone https://github.com/tty228/luci-app-serverchan.git package/danxiaonuo/luci-app-serverchan
 
 # 增加openwet常用软件包
