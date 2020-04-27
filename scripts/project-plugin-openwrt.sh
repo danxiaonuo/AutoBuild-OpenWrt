4282fc2cc6ef4938952d8b2dac2cce6f92ffbd3d
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-04-27 20:07:44 +0800

    Update project-plugin-openwrt.sh

--- scripts/project-plugin-openwrt.sh
+++ scripts/project-plugin-openwrt.sh
@@ -3,6 +3,9 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git package/danxiaonuo/l
 # smartdns
 svn co https://github.com/pymumu/smartdns/trunk/package/openwrt package/danxiaonuo/smartdns
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-smartdns package/danxiaonuo/luci-app-smartdns
+# Server酱
+rm -rf package/ctcgfw/luci-app-serverchan
+git clone https://github.com/tty228/luci-app-serverchan.git package/danxiaonuo/luci-app-serverchan
 # 删除 r8168 driver
 rm -rf package/ctcgfw/r8168
 # 删除 phicomm-k3screenctrl
