c137426192370d8687c1ea83e84d9a48eb3d7b4e
danxiaonuo <danxiaonuo@danxiaonuo.me>
2020-04-27 23:41:59 +0800

    Update project-plugin-openwrt.sh

--- scripts/project-plugin-openwrt.sh
+++ scripts/project-plugin-openwrt.sh
@@ -6,6 +6,12 @@ svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-
 # Server酱
 rm -rf package/ctcgfw/luci-app-serverchan
 git clone https://github.com/tty228/luci-app-serverchan.git package/danxiaonuo/luci-app-serverchan
+# 增加acl访问权限设置
+curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoBuild-OpenWrt/master/server/acl.d/luci-app-smartdns.json > package/base-files/files/usr/luci-app-smartdns.json
+curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoBuild-OpenWrt/master/server/acl.d/luci-app-serverchan.json > package/base-files/files/usr/luci-app-serverchan.json
+curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoBuild-OpenWrt/master/server/acl.d/luci-app-frpc.json > package/base-files/files/usr/luci-app-frpc.json
+curl -fsSL https://raw.githubusercontent.com/danxiaonuo/AutoBuild-OpenWrt/master/server/acl.d/luci-app-webadmin.json > package/base-files/files/usr/luci-app-webadmin.json
+sed -i '/exit 0/i\# 增加acl访问权限设置\nmv /usr/*.json /usr/share/rpcd/acl.d/' package/danxiaonuo/default-settings/files/zzz-default-settings
 # 删除 r8168 driver
 rm -rf package/ctcgfw/r8168
 # 删除 phicomm-k3screenctrl
